f7a74530d61a432476a75198aaa59bc598ce5ac1
alexshirley <atshgf@mst.edu>
2015-03-06 22:29:49 -0600

    Differential is working

--- src/mst/lejos/cpe5170/rtos_gt/RTOS_GT.java
+++ src/mst/lejos/cpe5170/rtos_gt/RTOS_GT.java
@@ -26,10 +26,13 @@ public class RTOS_GT {
 	static NXTRegulatedMotor steeringMotor = Motor.A;
 	static NXTRegulatedMotor driveMotor_Driver = Motor.B;
 	static NXTRegulatedMotor driveMotor_Passen = Motor.C;
-	static int leftMax = -110;
-	static int rightMax = 110;
-	static float width = 14; //14 cm
-	static float length = 22; //22 cm
+	final static int leftMax = -105;
+	final static int rightMax = 105;
+	final static float width = 14; //14 cm
+	final static float length = 22; //22 cm
+	static int steeringDegree = 0;
+	static int driveSpeed = 600;
+	static boolean differentialFlag = true; 
 	
 	public static void main(String[] args) throws Exception 
 	{
@@ -37,32 +40,62 @@ public class RTOS_GT {
 	
 	
 		while(!Button.ESCAPE.isDown()) {
-			steeringMotor.rotateTo(leftMax);
-			steeringMotor.rotateTo(rightMax);
-			driveMotor_Driver.stop();
-			driveMotor_Passen.flt();
+			steeringDegree = leftMax;
+			steeringMotor.rotateTo(steeringDegree);
+			drive(driveSpeed);
+			Thread.sleep(500);
+			steeringDegree = rightMax;
+			steeringMotor.rotateTo(steeringDegree);
+			drive(driveSpeed);
+			Thread.sleep(500);
 			//LCD.clear();
 			//LCD.drawString("Degrees", 0, 0);
 			//LCD.drawInt(steeringMotor.getTachoCount(), 0, 1);
-			//Thread.sleep(500);
 		}
 		steeringMotor.rotateTo(0);
+		driveMotor_Driver.setSpeed(0);
+		driveMotor_Passen.setSpeed(0);
 	}
 		
-	public float differential(){
-		if ((motor.side == driveMotor_Driver && steering.degree >= 0) or (motor.side == driveMotor_Passen && steering.degree >= 0))
+	public static float differential(){
+		if (steeringDegree == 0)
 				{
 					return (float) 1.0;
 				}
-				else	{
-						float pivotDistance = length / tan(abs(steering.degree)* Math.PI/180);
+				else
+				{
+					float pivotDistance = (float) (length / Math.tan(Math.abs(steeringDegree)* Math.PI/180));
 					float innerWheel = (float) (pivotDistance - width/2.0);
-	        float outerWheel = (float) (pivotDistance + width/2.0);
-			return (float) (innerWheel/outerWheel);
-		}
+					float outerWheel = (float) (pivotDistance + width/2.0);
+					return (float) (innerWheel/outerWheel);
+				}
 	//This section of code is heavily influenced by Vehicle safety systems
-	//	implemented on a LEGO car by J. Wortmann Et. al.
+	//	implemented on a LEGO car by J. Wortmann Et. al.	
+	}
 	
+	public static void drive(int driveSpeed)
+	{
+		if (differentialFlag)
+		{
+			if (steeringDegree >= 0)
+			{
+				driveMotor_Passen.setSpeed(driveSpeed);
+				driveMotor_Driver.setSpeed(differential() * driveSpeed);
+			}
+			else
+			{
+				driveMotor_Driver.setSpeed(driveSpeed);
+				driveMotor_Passen.setSpeed(differential() * driveSpeed);
+			}
+		}
+		else
+		{
+			driveMotor_Driver.setSpeed(driveSpeed);
+			driveMotor_Passen.setSpeed(driveSpeed);
+		}
+		driveMotor_Driver.backward(); //side effect of the motors
+		driveMotor_Passen.backward();
+		
 	}
 	
 }
@@ -74,7 +107,11 @@ public class RTOS_GT {
  * 				start ----> Steering ---------------> accel/decel --------------------------> terminus
  * 															|----------> slipping w/ decel------^
  * 
+ * 
+ * 
  * 				start ----> bluetooth control receipt ---> terminus
+ * 
+ * 
  * 
  * 									|---------------------> Initiate brake--------v
  * 				start ----> collision detection -------------------------------> terminus
