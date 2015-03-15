ca3f8da703a3fe95d72ff21ba6a4404b4f055363
alexshirley <atshgf@mst.edu>
2015-03-15 14:25:22 -0500

    Adding Support Files for MindDroid stuff, also updating RTOS_GT

--- src/mst/lejos/cpe5170/rtos_gt/RTOS_GT.java
+++ src/mst/lejos/cpe5170/rtos_gt/RTOS_GT.java
@@ -8,8 +8,9 @@ import java.lang.Math;
 
 
 
-
-
+import lejos.nxt.*;
+import lejos.nxt.comm.*;
+import lejos.robotics.*;
 import lejos.robotics.RegulatedMotor;
 import lejos.nxt.Motor;
 import lejos.nxt.NXTRegulatedMotor;
@@ -21,41 +22,61 @@ import lejos.nxt.addon.SensorSelector;
 import lejos.nxt.comm.BTConnection;
 import lejos.nxt.comm.Bluetooth;
 
-public class RTOS_GT {
+public class RTOS_GT //implements CommandPerformer//
+{
 	
 	static NXTRegulatedMotor steeringMotor = Motor.A;
 	static NXTRegulatedMotor driveMotor_Driver = Motor.B;
 	static NXTRegulatedMotor driveMotor_Passen = Motor.C;
 	final static int leftMax = -105;
 	final static int rightMax = 105;
-	final static double width = 12.5; //12.5 cm
-	final static double length = 17.5; //17.5 cm
+	final static float width = 12.5f; //12.5 cm
+	final static float length = 17.5f; //17.5 cm
 	static int steeringDegree = 0;
 	static int driveSpeed = 600;
 	static boolean differentialFlag = true;
 	static int turnThreshold = 20;
+	static int[] accelConstants = {0,1,1,2,3,5,9,15,25,40,67,110,181,299,493,812,1339,2207,3639,6000,9892};
+	static float[] speedConstants = {1000,600,364,220,134,81,49,30,18,11,6.7f,4,2.5f,1.5f,0.9f,0.5f,0.3f,0.2f,0.1f,0.1f,0};
+	static int powerPointer = 0;
 	
 	public static void main(String[] args) throws Exception 
 	{
 		//I tried to create a calibration system, but the gears slip too much
+		driveMotor_Driver.setSpeed(1000);
+		driveMotor_Passen.setSpeed(1000);
 	
 	
 		while(!Button.ESCAPE.isDown()) {
-			steeringDegree = -10;
-			steeringMotor.rotateTo(steeringDegree);
-			drive(driveSpeed);
+			
+			powerTest(accelConstants[powerPointer]);
+			driveMotor_Driver.backward();
+			driveMotor_Passen.backward();
 			Thread.sleep(500);
-			steeringDegree = 10;
-			steeringMotor.rotateTo(steeringDegree);
-			drive(driveSpeed);
-			Thread.sleep(500);
+			LCD.drawInt(accelConstants[powerPointer], 0, 1);
+			powerPointer++;
+			if (accelConstants[powerPointer] > 6000)
+			{
+				powerPointer = 0;
+			}
+					
+			//steeringDegree = -10;
+			//steeringMotor.rotateTo(steeringDegree);
+			//drive(driveSpeed);
+			//Thread.sleep(500);
+			//steeringDegree = 10;
+			//steeringMotor.rotateTo(steeringDegree);
+			//drive(driveSpeed);
+			//Thread.sleep(500);
 			//LCD.clear();
 			//LCD.drawString("Degrees", 0, 0);
 			//LCD.drawInt(steeringMotor.getTachoCount(), 0, 1);
 		}
 		steeringMotor.rotateTo(0);
-		driveMotor_Driver.setSpeed(0);
-		driveMotor_Passen.setSpeed(0);
+		driveMotor_Driver.setAcceleration(6000);
+		driveMotor_Passen.setAcceleration(6000);
+		driveMotor_Driver.stop();
+		driveMotor_Passen.stop();
 	}
 		
 	public static float differential(){
@@ -70,8 +91,8 @@ public class RTOS_GT {
 					float outerWheel = (float) (pivotDistance + width/2.0);
 					return (float) (innerWheel/outerWheel);
 				}
-	//This section of code is heavily influenced by Vehicle safety systems
-	//	implemented on a LEGO car by J. Wortmann Et. al.	
+	//This section of code is heavily influenced by "Vehicle safety systems
+	//	implemented on a LEGO car" by J. Wortmann Et. al.	
 	}
 	
 	public static void drive(int driveSpeed)
@@ -99,6 +120,24 @@ public class RTOS_GT {
 		driveMotor_Passen.backward();
 		
 	}
+	
+	public static void powerTest(int powerConst)
+	{
+		driveMotor_Driver.setAcceleration(powerConst);
+		driveMotor_Passen.setAcceleration(powerConst);
+		driveMotor_Driver.setSpeed(powerConst);
+		driveMotor_Passen.setSpeed(powerConst);
+		
+	}
+
+//	@Override
+//	public void performCommand(int commandNr, byte[] parameter) {
+//		switch (commandNr)
+//		case
+		
+		// TODO Auto-generated method stub
+		
+//	}
 	
 }
 
